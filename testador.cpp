39d409a1894de9b0f80bdcf2e6289f0c72d799bc
r4cky70 <rodrigo.estrela.sousa@ccc.ufcg.edu.br>
2019-09-08 02:36:09 -0300

    renamming soltions's file from 'teste' to 'solve'

--- testador.cpp
+++ testador.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
   system("g++ gerador.cpp -std=c++14 -o C:\\Users\\ESRELA\\Documents\\CProgramming\\TestadorW\\Runs\\ger");
   system("g++ brute.cpp -std=c++14 -o C:\\Users\\ESRELA\\Documents\\CProgramming\\TestadorW\\Runs\\bru");
-  system("g++ teste.cpp -std=c++14 -o C:\\Users\\ESRELA\\Documents\\CProgramming\\TestadorW\\Runs\\tes");
+  system("g++ solve.cpp -std=c++14 -o C:\\Users\\ESRELA\\Documents\\CProgramming\\TestadorW\\Runs\\tes");
   while(true){
     system("C:\\Users\\ESRELA\\Documents\\CProgramming\\TestadorW\\Runs\\ger");
     system("C:\\Users\\ESRELA\\Documents\\CProgramming\\TestadorW\\Runs\\bru");
